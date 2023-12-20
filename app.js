95bd6d5eff47221bb980442fb076ac390851a40c
Manjot Bhasin <manny.bhasin@gmail.com>
2023-12-20 17:24:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+const gencompChoice = ()=> {
+    const options = ["rock","paper","scissors"];
+    const optInd = Math.floor(Math.random() * 3);
+    return options[optInd];
+ 
+};
+
+const drawGame = ()=> {
+    console.log("game was draw.");
+    msg.innerText  = "Game was Draw, Play Again!";
+    msg.style.backgroundColor = "black";
+};
+
+const showWinner = (userWin, userChoice, compChoice) => {
+
+    if(userWin) {
+        userScore++;
+        userScorePara.innerText = userScore;
+        msg.innerText  = `You Win! ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor = "green";
+
+    } else {
+        compScore++;
+        compScorePara.innerText = compScore;
+        msg.innerText  = `You lose. ${compChoice} beats ${userChoice}`;
+        msg.style.backgroundColor = "red";
+    }
+};
+
+
+const playGame = (userChoice) => {
+    console.log("user choice = ", userChoice);
+
+    const compChoice =  gencompChoice();
+    console.log("Comp choice = ", compChoice);
+
+
+    if (userChoice === compChoice) {
+        drawGame();
+    }
+    else {
+        let userWin = true;
+        if(userChoice === "rock") {
+            userWin = compChoice === "paper" ? false: true;
+        } else if(userChoice === "paper") {
+            userWin = compChoice === "scissors" ? false : true;
+        } else {
+            userwin = compChoice === "rock" ? false: true;
+        }
+
+        showWinner(userWin, userChoice, compChoice);
+
+    }
+};
+
+
+choices.forEach((choice) => {
+
+    choice.addEventListener("click",()=> {
+
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+       
+    });
+});
+
+
+
